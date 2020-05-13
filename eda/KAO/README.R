19a51b31140b049d547fbc1fc42119c78f5402a0
kappacoo <kappacoo@gmail.com>
2020-05-12 22:37:57 -0500

    # Updated README file to contain information about new files 2020-05-12

--- eda/KAO/README.R
+++ eda/KAO/README.R
@@ -1,1 +1,13 @@
-## Readme
+##### README ###### 
+
+## "01_KAO_Establishing_connection_to_db_extracting_timeStamp.R"
+# description: Establishes DB connection using RSQLite package and 
+#     fetches time stamp information for Raw files.
+# Relevant Issue(s): 
+# date created: 5/12/20
+# date last modified: 5/12/20
+# input:
+#  - Covid-19 Study DB.sqlite
+# output:
+#  - 
+
