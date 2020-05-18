5c7cd7df9cf9d56f668dee5d55ce84fd7678848c
Ian Miller <ijmiller2@wisc.edu>
2020-05-18 15:23:03 -0500

    #1 Python code for SQLite queries.

--- src/db_queries/db_queries.py
+++ src/db_queries/db_queries.py
@@ -1,3 +1,33 @@
 
-import antigravity
-import pandas
+# Import create_engine
+from sqlalchemy import create_engine, MetaData, Table, select
+
+# Create an engine that connects to the census.sqlite file: engine
+engine = create_engine('sqlite:///data/SQLite Database/Covid-19 Study DB.sqlite')
+
+# Establish connection
+connection = engine.connect()
+
+# Print table names
+print(engine.table_names())
+
+# Instantiate metadata table
+metadata = MetaData()
+
+# Reflect the census table from the engine: census
+metabolomics_measurements = Table('metabolomics_measurements', metadata, autoload=True, autoload_with=engine)
+
+# Print the column names
+print(metabolomics_measurements.columns.keys())
+
+# Print full metadata of metabolomics_measurements
+print(repr(metadata.tables['metabolomics_measurements']))
+
+# SQL alchemy select all columns from metabolomics_measurements
+stmt = select([metabolomics_measurements])
+results = connection.execute(stmt).fetchall()
+
+# Direct SQL query
+stmt = 'SELECT * FROM metabolomics_measurements'
+result_proxy = connection.execute(stmt)
+results = result_proxy.fetchall()
