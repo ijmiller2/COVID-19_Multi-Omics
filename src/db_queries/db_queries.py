feeea19bf517fa74bb7c8f50117352611dd1a03d
Ian Miller <ijmiller2@wisc.edu>
2020-05-26 12:40:53 -0500

    Added get_omics_data() function to db_queries.py. Removed previous metabolomics-specific query code.

--- src/db_queries/db_queries.py
+++ src/db_queries/db_queries.py
@@ -1,33 +1,94 @@
+from sqlalchemy import create_engine, MetaData, Table, select, join
+import pandas as pd
 
-# Import create_engine
-from sqlalchemy import create_engine, MetaData, Table, select
+# SQLite path
+db_path = 'sqlite:///../../data/SQLite Database/20200525/Covid-19 Study DB.sqlite'
 
-# Create an engine that connects to the census.sqlite file: engine
-engine = create_engine('sqlite:///data/SQLite Database/Covid-19 Study DB.sqlite')
+def get_omics_data(with_metadata=False, dataset="proteomics"):
 
-# Establish connection
-connection = engine.connect()
+    omics_id_dict = {
+        "proteomics":1,
+        "lipidomics":2,
+        "metabolomics":3,
+        "transcriptomics":4
+    }
 
-# Print table names
-print(engine.table_names())
+    omics_id = omics_id_dict[dataset]
 
-# Instantiate metadata table
-metadata = MetaData()
+    # Create an engine that connects to the Covid-19 Study DB.sqlite file: engine
+    engine = create_engine(db_path)
 
-# Reflect the census table from the engine: census
-metabolomics_measurements = Table('metabolomics_measurements', metadata, autoload=True, autoload_with=engine)
+    # Establish connection
+    connection = engine.connect()
 
-# Print the column names
-print(metabolomics_measurements.columns.keys())
+    # pull table into df
+    query = "SELECT * from {}_measurements".format(dataset)
+    omics_measurements_df = pd.read_sql_query(query, connection)
 
-# Print full metadata of metabolomics_measurements
-print(repr(metadata.tables['metabolomics_measurements']))
+    # pull table into df
+    query = "SELECT * from {}_runs".format(dataset)
+    omics_runs_df = pd.read_sql_query(query, connection)
 
-# SQL alchemy select all columns from metabolomics_measurements
-stmt = select([metabolomics_measurements])
-results = connection.execute(stmt).fetchall()
+    # pull table into df
+    query = "SELECT * from rawfiles WHERE ome_id={} AND sample_ID<>-1 and keep=1".format(omics_id)
+    rawfiles_df = pd.read_sql_query(query, connection)
 
-# Direct SQL query
-stmt = 'SELECT * FROM metabolomics_measurements'
-result_proxy = connection.execute(stmt)
-results = result_proxy.fetchall()
+    # pull table into df
+    deidentified_patient_metadata_df = pd.read_sql_query("SELECT * from deidentified_patient_metadata", connection)
+
+    # make sure the merge by columns are all the same type -> pandas seems sensitive to this
+    omics_measurements_df = omics_measurements_df.astype({'replicate_id': 'int32'})
+    omics_runs_df = omics_runs_df.astype({'replicate_id': 'int32', 'rawfile_id': 'int32'})
+    rawfiles_df = rawfiles_df.astype({'rawfile_id': 'int32', 'sample_id': 'int32'})
+    deidentified_patient_metadata_df = deidentified_patient_metadata_df.astype({'sample_id': 'int32'})
+
+    joined_df = omics_measurements_df\
+                .join(omics_runs_df.set_index('replicate_id'), on='replicate_id')\
+                .join(rawfiles_df.set_index('rawfile_id'), on='rawfile_id')\
+                .join(deidentified_patient_metadata_df.set_index('sample_id'), on='sample_id')
+
+    # drop samples that are missing COVID or ICU status
+    joined_df.dropna(subset=['ICU_1','COVID'], inplace=True)
+
+    # pivot to wide format
+    wide_df = joined_df.pivot_table(index='sample_id', columns='biomolecule_id', values='normalized_abundance')
+    wide_df.columns = [str(col) for col in wide_df.columns]
+
+    query = "SELECT * from biomolecules WHERE omics_id={}".format(omics_id)
+    # get biomolecule names
+    biomolecules_df = pd.read_sql_query(query, connection)
+
+    # close DB connection
+    connection.close()
+
+    # build biomolecule name dict and drop list
+    biomolecule_name_dict = {}
+    biomolecule_drop_list = []
+    for index, row in biomolecules_df.iterrows():
+        biomolecule_id = str(row['biomolecule_id'])
+        standardized_name = row['standardized_name']
+        biomolecule_name_dict[biomolecule_id] = standardized_name
+
+        keep = row['keep']
+        if keep!="1":
+            biomolecule_drop_list.append(biomolecule_id)
+
+    # drop biomolecules
+    wide_df.drop(biomolecule_drop_list, axis=1, inplace=True)
+
+    # replace wide_df column names
+    new_col_names = []
+    for col in wide_df.columns:
+        new_col_names.append(biomolecule_name_dict[str(col)])
+    wide_df.columns = new_col_names
+
+    # record quant value range
+    quant_value_range = wide_df.shape[1]
+
+    # optional return matrix with clinical metadata
+    if with_metadata:
+
+        combined_df = wide_df.join(deidentified_patient_metadata_df.set_index('sample_id'), on='sample_id')#.dropna()
+        return combined_df, quant_value_range
+
+    return wide_df, quant_value_range
