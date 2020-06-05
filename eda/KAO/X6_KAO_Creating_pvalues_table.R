67c6aadd4b5a48ca47e7e83a8ebf62672b3bf628
kappacoo <kappacoo@gmail.com>
2020-06-05 07:35:20 -0500

    Edited pvalue table to include effect size.

--- eda/KAO/X6_KAO_Creating_pvalues_table.R
+++ eda/KAO/X6_KAO_Creating_pvalues_table.R
@@ -26,8 +26,19 @@ compare_lr <- function(biomolecule_id, formula_null, formula_test, data, return 
         lrt_lratio <- lrt$F[2]
         lrt_pvalue <- lrt$`Pr(>F)`[2]
       }
-  if(return == 'pvalue') lrt_pvalue else lrt_lratio
+  if(return == 'pvalue') lrt_pvalue else if(return == 'both') c(lratio = lrt_lratio,pvalue = lrt_pvalue)
 }
+
+# this function is currently usign SS type III calculation. But probably we should be doing SS type I. Will edit in future. 
+# lm_coefficients <- function(biomolecule_id, formula_test, data, return_first = T, return_category = NULL){
+#   lm_formula_test <- lm(formula_test, data = data[data$biomolecule_id == biomolecule_id, ])
+#   if(return_first == T){
+#     c(coeficient =  summary(lm_formula_test)$coefficients[2,3], pvalue = summary(lm_formula_test)$coefficients[2,4])
+#   } else if (!is.null(return_category) & return_category %in% row.names(summary(lm_formula_test)$coefficients)){
+#     index <- match(return_category, row.names(summary(lm_formula_test)$coefficients))
+#     c(coeficient =  summary(lm_formula_test)$coefficients[index,3], pvalue = summary(lm_formula_test)$coefficients[index,4])
+#   }
+# }
 
 ###### Applying this function across all features ########
 
@@ -38,9 +49,7 @@ con <- dbConnect(RSQLite::SQLite(), dbname = "P:/All_20200428_COVID_plasma_multi
 
 dbListTables(con)
 
-
-##### 
-df_metabolites<- dbGetQuery(con, "SELECT deidentified_patient_metadata.sample_id, normalized_abundance, metabolomics_measurements.biomolecule_id, COVID, Age_less_than_90, Gender, ICU_1
+df_metabolites<- dbGetQuery(con, "SELECT deidentified_patient_metadata.sample_id, normalized_abundance, metabolomics_measurements.biomolecule_id, COVID, Age_less_than_90, Gender, ICU_1, DM, Charlson_score, SOFA
            FROM metabolomics_measurements
            INNER JOIN metabolomics_runs ON metabolomics_runs.replicate_id = metabolomics_measurements.replicate_id
            INNER JOIN rawfiles ON rawfiles.rawfile_id = metabolomics_runs.rawfile_id
@@ -50,7 +59,7 @@ df_metabolites<- dbGetQuery(con, "SELECT deidentified_patient_metadata.sample_id
            AND biomolecules.keep = '1'
            ")
 
-df_lipids<- dbGetQuery(con, "SELECT deidentified_patient_metadata.sample_id, normalized_abundance, lipidomics_measurements.biomolecule_id, COVID, Age_less_than_90, Gender, ICU_1
+df_lipids<- dbGetQuery(con, "SELECT deidentified_patient_metadata.sample_id, normalized_abundance, lipidomics_measurements.biomolecule_id, COVID, Age_less_than_90, Gender, ICU_1, DM, Charlson_score, SOFA
            FROM lipidomics_measurements
            INNER JOIN lipidomics_runs ON lipidomics_runs.replicate_id = lipidomics_measurements.replicate_id
            INNER JOIN rawfiles ON rawfiles.rawfile_id = lipidomics_runs.rawfile_id
@@ -61,7 +70,7 @@ df_lipids<- dbGetQuery(con, "SELECT deidentified_patient_metadata.sample_id, nor
            ")
 
 
-df_proteins<- dbGetQuery(con, "SELECT deidentified_patient_metadata.sample_id, normalized_abundance, proteomics_measurements.biomolecule_id, COVID, Age_less_than_90, Gender, ICU_1
+df_proteins<- dbGetQuery(con, "SELECT deidentified_patient_metadata.sample_id, normalized_abundance, proteomics_measurements.biomolecule_id, COVID, Age_less_than_90, Gender, ICU_1, DM, Charlson_score, SOFA
            FROM proteomics_measurements
            INNER JOIN proteomics_runs ON proteomics_runs.replicate_id = proteomics_measurements.replicate_id
            INNER JOIN rawfiles ON rawfiles.rawfile_id = proteomics_runs.rawfile_id
@@ -75,29 +84,25 @@ dbDisconnect(con)
 
 #### Creating dataframe to hold pvalues #######
 
-df <- rbind(df_metabolites,df_lipids, df_proteins)
-
-df <- df[df$sample_id != 54, ]
-
 df_pvalues <- data.frame(biomolecule_id = unique(df$biomolecule_id), test = "LR_test", comparison = "COVID_vs_NONCOVID", confounders = "ICU_1;Gender;Age_less_than_90")
-
 
 p_value <- apply(df_pvalues, 1, function(x)  
             compare_lr(as.numeric(x[1]), formula_null = normalized_abundance ~ ICU_1 + Gender + Age_less_than_90, 
              formula_test = normalized_abundance ~ COVID * ICU_1 + Gender + Age_less_than_90,
-             data = df, return = 'pvalue'))
+             data = df, return = 'both'))
 
-df_pvalues$p_value <- p_value
+df_pvalues$effect_size <- p_value[1,]
+df_pvalues$p_value <- p_value[2,]
 df_pvalues$q_value <- p.adjust(df_pvalues$p_value, method = "fdr")
 
 df_pvalues <- cbind(pvalue_id = row.names(df_pvalues), df_pvalues)
 
 hist(df_pvalues$p_value)
 
-#### Establish a connection to the DB #####
+#### Establish a connection to the DB
 con <- dbConnect(RSQLite::SQLite(), dbname = "P:/All_20200428_COVID_plasma_multiomics/SQLite Database/Covid-19 Study DB.sqlite")
 
-#### write table to DB ####
+#### write table to DB 
 
 dbWriteTable(con, "pvalues", df_pvalues, overwrite = T)
 
@@ -112,18 +117,18 @@ df_gender <- df[df$Gender != "", ]
 
 df_pvalues_gender <- data.frame(biomolecule_id = unique(df$biomolecule_id), test = "LR_test", comparison = "GENDER", confounders = "COVID;ICU_1;Age_less_than_90")
 
-df_pvalues_gender$p_value <- apply(df_pvalues_gender, 1, function(x)  
+p_value <- apply(df_pvalues_gender, 1, function(x)  
 compare_lr(as.numeric(x[1]), formula_null = normalized_abundance ~ COVID + ICU_1 + Age_less_than_90, 
              formula_test = normalized_abundance ~ COVID + ICU_1 + Gender + Age_less_than_90,
-             data = df_gender, return = 'pvalue'))
+             data = df_gender, return = 'both'))
+df_pvalues_gender$effect_size <- p_value[1,]
+df_pvalues_gender$p_value <- p_value[2,]
 
 hist(df_pvalues_gender$p_value, breaks = 100, main = 'Histogram of pvalues +/- gender')
 
 df_pvalues_gender$q_value <- p.adjust(df_pvalues_gender$p_value, method = "fdr")
 
 df_pvalues_gender <- cbind(pvalue_id = seq(nrow(pvalues)+1, length.out = nrow(df_pvalues_gender), by =1 ), df_pvalues_gender)
-
-table(df_pvalues_gender$q_value < 0.05)
 
 ## Append gender Pvalues to db 
 ## Establish a connection to the DB 
@@ -139,14 +144,17 @@ pvalues <- dbReadTable(con, "pvalues")
 # disconnect
 dbDisconnect(con) 
 
-####### Pvalues w/ age ######
+####### P-values w/ age ######
 
 df_pvalues_age <- data.frame(biomolecule_id = unique(df$biomolecule_id), test = "LR_test", comparison = "Age_less_than_90", confounders = "COVID;ICU_1;Gender")
 
-df_pvalues_age$p_value <- apply(df_pvalues_age, 1, function(x)  
+p_value <- apply(df_pvalues_age, 1, function(x)  
   compare_lr(as.numeric(x[1]), formula_null = normalized_abundance ~ COVID + ICU_1 + Gender, 
              formula_test = normalized_abundance ~ COVID + ICU_1 + Gender + Age_less_than_90,
-             data = df, return = 'pvalue'))
+             data = df, return = 'both'))
+
+df_pvalues_age$effect_size <- p_value[1,]
+df_pvalues_age$p_value <- p_value[2,]
 
 hist(df_pvalues_age$p_value, breaks = 100, main = 'Histogram of pvalues +/- age')
 
@@ -169,14 +177,16 @@ pvalues <- dbReadTable(con, "pvalues")
 # disconnect
 dbDisconnect(con) 
 
-####### Pvalues w/ ICU status 
+####### P-values w/ ICU status ######
 
 df_pvalues_icu <- data.frame(biomolecule_id = unique(df$biomolecule_id), test = "LR_test", comparison = "ICU", confounders = "COVID;Gender;Age_less_than_90")
 
-df_pvalues_icu$p_value <- apply(df_pvalues_icu, 1, function(x)  
+p_value <- apply(df_pvalues_icu, 1, function(x)  
   compare_lr(as.numeric(x[1]), formula_null = normalized_abundance ~ COVID + Gender + Age_less_than_90, 
              formula_test = normalized_abundance ~ COVID + ICU_1 + Gender + Age_less_than_90,
-             data = df, return = 'pvalue'))
+             data = df, return = 'both'))
+df_pvalues_icu$effect_size <- p_value[1,]
+df_pvalues_icu$p_value <- p_value[2,]
 
 hist(df_pvalues_icu$p_value, breaks = 100, main = 'Histogram of pvalues +/- ICU')
 
@@ -200,14 +210,16 @@ pvalues <- dbReadTable(con, "pvalues")
 dbDisconnect(con) 
 
 
-####### Pvalues w/ COVID ICU interaction 
+####### Pvalues w/ COVID ICU interaction #####
 
 df_pvalues_interaction <- data.frame(biomolecule_id = unique(df$biomolecule_id), test = "LR_test", comparison = "COVID ICU interaction", confounders = "ICU_1;Gender;Age_less_than_90")
 
-df_pvalues_interaction$p_value <- apply(df_pvalues_interaction, 1, function(x)  
+p_value <- apply(df_pvalues_interaction, 1, function(x)  
   compare_lr(as.numeric(x[1]), formula_null = normalized_abundance ~ COVID + ICU_1 + Gender + Age_less_than_90, 
              formula_test = normalized_abundance ~ COVID * ICU_1 + Gender + Age_less_than_90,
-             data = df, return = 'pvalue'))
+             data = df, return = 'both'))
+df_pvalues_interaction$effect_size <- p_value[1,]
+df_pvalues_interaction$p_value <- p_value[2,]
 
 hist(df_pvalues_interaction$p_value, breaks = 100, main = 'Histogram of pvalues +/- COVID ICU interaction')
 
@@ -228,4 +240,3 @@ pvalues <- dbReadTable(con, "pvalues")
 
 # disconnect
 dbDisconnect(con) 
-
